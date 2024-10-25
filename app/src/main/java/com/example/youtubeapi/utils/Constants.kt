5e83d70d74e35af888ab7640cf4cb368f2ea942b
DezireM <deziremaatkabylova@gmail.com>
2024-10-25 23:24:56 +0600

    .

--- app/src/main/java/com/example/youtubeapi/utils/Constants.kt
+++ app/src/main/java/com/example/youtubeapi/utils/Constants.kt
@@ -1,10 +1,10 @@
 package com.example.youtubeapi.utils
 
 object Constants {
-    const val API_KEY = "AIzaSyA_rWWOXD8ky4d6gR6MZ_Gq-Wig33vWRV0"
+    const val API_KEY = "AIzaSyDoOYDPDPOdH3cKhZxgOWYWc0xEEdJhs0k"
     const val PART = "snippet,contentDetails"
     const val MAX_RESULTS = 20
-    const val CHANNEL_ID = "UCCXF68Da_ndcmvv_9OG75Cw"
+    const val CHANNEL_ID = "@acceptthyself"
     const val TIMEOUT_DURATION = 10L
-    const val BASE_URL = "https://www.googleapis.com/youtube/v3/"
+    const val BASE_URL = "https://www.youtube.com/watch?v=X9IAVEaADz0"
 }
